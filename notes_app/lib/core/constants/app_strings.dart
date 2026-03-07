0dcd1c812418e0b43c23db554309c3f4c8a86934
Remon <Remon132004@gmail.com>
2026-03-07 19:53:52 +0200

    App Done

--- notes_app/lib/core/constants/app_strings.dart
+++
@@ -1,5 +0,0 @@
-class AppStrings{
-  static String title='Title';
-  static String content='Content';
-  static String notesBox='notes_box';
-}
--- notes_app/lib/core/routing/routes.dart
+++
@@ -1,4 +0,0 @@
-class AppRoutes{
-  static const String homeScreen='/homeScreen';
-  static const String editScreen='/editScreen';
-}
