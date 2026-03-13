bfe43b01df876c3204b0b49fadfc15be52b68538
Remon <Remon132004@gmail.com>
2026-03-13 15:05:46 +0200

    Add constants

---
+++ notes/lib/core/constants/app_colors.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final Color primaryColor = Colors.black;
+  static final Color secondaryColor = Color(0xff342a18);
+  static final Color? noteTextColor = Colors.cyan[200];
+  static const List<Color> noteColors = [
+    Color(0xFF53EDD8),
+    Color(0xFFFFF176),
+    Color(0xFFFFAB91),
+    Color(0xFFCF94DA),
+    Color(0xFF81DEEA),
+    Color(0xFFE7ED9B),
+    Color(0xFFFF8A80),
+    Color(0xFFA5D6A7),
+  ];
+}
---
+++ notes/lib/core/routing/app_router.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/core/routing/routes.dart';
+import 'package:notes/features/note/data/models/note_model.dart';
+import 'package:notes/features/note/presentation/cubit/edit_note_cubit/edit_note_cubit.dart';
+
+import '../../features/note/data/repo/NotesRepo.dart';
+import '../../features/note/presentation/screens/edit_note_screen.dart';
+import '../../features/note/presentation/screens/note_screen.dart';
+
+class AppRouter {
+  static Route generateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case AppRoutes.homeScreen:
+        return MaterialPageRoute(
+          builder: (context) {
+            return HomeScreen();
+          },
+        );
+      case AppRoutes.editScreen:
+        final args = settings.arguments as Map<String, dynamic>;
+        final note = args['note'] as NoteModel;
+        return MaterialPageRoute(
+          builder: (context) {
+            return BlocProvider(
+              create: (context) => EditNoteCubit(NotesRepo()),
+              child: EditNoteScreen(note: note,),
+            );
+          },
+        );
+      default:
+        return MaterialPageRoute(
+          builder: (context) {
+            return Scaffold(body: Center(child: Text("No Route Defined ")));
+          },
+        );
+    }
+  }
+}
