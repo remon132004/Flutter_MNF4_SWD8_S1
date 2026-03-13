d62b9938f9142454f4a1874ca8705a0da04c018b
Remon <Remon132004@gmail.com>
2026-03-13 15:06:55 +0200

    Add notes cubit

---
+++ notes/lib/features/note/presentation/cubit/notes_cubit/notes_cubit.dart
@@ -0,0 +1,29 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes/features/note/data/models/note_model.dart';
+import 'package:notes/features/note/data/repo/NotesRepo.dart';
+
+part 'notes_state.dart';
+
+class NotesCubit extends Cubit<NotesState> {
+  NotesCubit(this.repo) : super(NotesInitial());
+  final NotesRepo repo;
+
+  void getNotes() {
+    emit(NotesLoading());
+    try {
+      final notes = repo.getNotes();
+      emit(NotesSuccess(notes));
+    } catch (e) {
+      emit(NotesFailure(e.toString()));
+    }
+  }
+
+  // Future<void> deleteNote(int index) async {
+  //   try {
+  //     await repo.deleteNote(index);
+  //     getNotes();
+  //   } catch (e) {
+  //     emit(NotesFailure(e.toString()));
+  //   }
+  // }
+}
---
+++ notes/lib/features/note/presentation/cubit/notes_cubit/notes_state.dart
@@ -0,0 +1,19 @@
+part of 'notes_cubit.dart';
+
+abstract class NotesState {}
+
+final class NotesInitial extends NotesState {}
+
+final class NotesLoading extends NotesState {}
+
+final class NotesSuccess extends NotesState {
+  final List<NoteModel> notes;
+
+  NotesSuccess(this.notes);
+}
+
+final class NotesFailure extends NotesState {
+  final String err;
+
+  NotesFailure(this.err);
+}
